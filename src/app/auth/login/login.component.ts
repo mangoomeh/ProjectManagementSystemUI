308c5a06620b5287443d5844942ca2238f7d0d6a
ruibing <rbruibingnet@gmail.com>
2022-05-10 11:55:08 +0800

    update login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  isValidated: boolean = false;
   loginForm = this.fb.group({
     email: ['', Validators.compose([Validators.required, Validators.email])],
     password: ['', Validators.required],
@@ -22,11 +23,22 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+
   login() {
+    if (!this.loginForm.valid) {
+      alert('invalid email / password')
+      return;
+    }
     this.auth.login(this.loginForm.value).subscribe({
       next: (res) => {
         console.log(res);
         this.router.navigate(['pms']);
+      },
+      error: (err) => {
+        console.log(err);
+        alert('something went wrong');
       },
     });
   }
