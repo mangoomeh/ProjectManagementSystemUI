ca96a3875c585f129e5ec3839227495f34e1ca8d
ruibing <rbruibingnet@gmail.com>
2022-05-12 00:27:35 +0800

    added logout

--- src/app/pms/pms.component.ts
+++ src/app/pms/pms.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pms',
   templateUrl: './pms.component.html',
-  styleUrls: ['./pms.component.scss']
+  styleUrls: ['./pms.component.scss'],
 })
 export class PmsComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['']);
   }
-
 }
